fba48b0df44bce3377b5d27a2b95ca43340bd520
Mikeywardle <mikeywardle13@gmail.com>
2019-08-18 23:09:16 +0100

    Inital Commit
    InitalCommit

---
+++ Assets/TileSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSetter : MonoBehaviour
+{
+
+    public Tilemap buildingMap;
+    public Tile toPlace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildingMap.SetTile(new Vector3Int(1, 1, 0), toPlace);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
